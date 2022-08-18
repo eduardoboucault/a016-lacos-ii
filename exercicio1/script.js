3c290b213c4f88b2b2cfa53bc9a38c5237a684c9
Eduardo Boucault <boucault.labenu@outlook.com>
2022-08-18 18:29:01 -0300

    Exercícios laços 2, aguardando feedback.

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,37 @@
 const array = [
-    [27, 4, 20, 13, 14],
-    [11, 15, 12, 8, 9],
-    [5, 5, 12, 16, 4],
-    [20, 33, 11, 12, 19],
-    [3, 3, 4, 5, 10]
+    [27, 4, 20, 13, 14],   //? Índice 0 - Jogador 1
+    [11, 15, 12, 8, 9],    //? Índice 1 - Jogador 2
+    [5, 5, 12, 16, 4],     //? Índice 2 - Jogador 3
+    [20, 33, 11, 12, 19],  //? Índice 3 - Jogador 4
+    [3, 3, 4, 5, 10]       //? Índice 4 - Jogador 5
 ]
+
+//? Laço in / of
+
+// let j  //? j de jogadores
+
+// let g  //? g de gols
+
+// for(j in array){  //? for in percorre índice
+
+//     let comprovante
+
+//     comprovante = `Jogador${Number(j)+1}: `
+
+//     for(g of array[j]){  //? for of percorre valores
+
+//         comprovante += `${g} `
+
+//     }
+   
+//     console.log(comprovante)
+
+// }
+
+//? Laço in direto
+
+for (i = 0; i < array.length; i++){
+
+    console.log(`Jogador ${i+1}: ${array[i]}`);
+
+}
